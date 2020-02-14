3f21b87726772bef76a25abfc07ca6398e00319d
ccop2 <crazysmileyapps@gmail.com>
2020-02-13 21:36:23 -0800

    fix

--- implement/applications/eve-online/eve-online-mining-bot/src/Bot.elm
+++ implement/applications/eve-online/eve-online-mining-bot/src/Bot.elm
@@ -529,7 +529,7 @@ firstAsteroidFromOverviewWindow =
 
 isShipTakingDamage : MemoryReading -> Bool
 isShipTakingDamage =
-    .shield >> ((shipUi.hitpointsAndEnergyMilli.shield // 10) < 80) >> Result.toMaybe
+    .shield >> ((ShipUi.hitpointsAndEnergyMilli.shield // 10) < 80) >> Result.toMaybe
     
 overviewWindowEntryIsInRange : OverviewWindowEntry -> Maybe Bool
 overviewWindowEntryIsInRange =
